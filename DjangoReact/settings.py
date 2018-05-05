521fb4a7e114dfa76a0b42f9ea5069093eed1550
hakemkadem <hakemk21@gmail.com>
2018-05-05 18:32:05 +0300

    Adding master Page contents

--- DjangoReact/settings.py
+++ DjangoReact/settings.py
@@ -151,4 +151,3 @@ STATIC_URL = '/static/'
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
