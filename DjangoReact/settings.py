c8790977cbb85a19672486053252ade6ebe093bc
hakemkadem <hakemk21@gmail.com>
2018-05-05 19:08:41 +0300

    Adding master Page contents

--- DjangoReact/settings.py
+++ DjangoReact/settings.py
@@ -151,3 +151,5 @@ STATIC_URL = '/static/'
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MEDIA_URL="/media/"
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
--- DjangoReact/urls.py
+++ DjangoReact/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
 from snippets.views import TestFromHomeIndex,PostList
-
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('snippets/',include('snippets.urls')),
@@ -10,3 +10,7 @@ urlpatterns = [
     path('TestIndex',TestFromHomeIndex,name="MainTest"),
     path('Post',PostList)
 ]
+
+urlpatterns+=static(settings.MEDIA_URL,
+                    documents_root= settings.MEDIA_ROOT)
+
